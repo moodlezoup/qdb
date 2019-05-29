32e090c246ccef0db2ac5f92e8db3a2b431dfcab
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2019-05-29 00:10:54 -0700

    Remove unittest dependency in tests

--- qdb/tests/test_dag.py
+++ qdb/tests/test_dag.py
@@ -1,4 +1,3 @@
-from unittest import TestCase
 import pytest
 
 import qdb
@@ -7,18 +6,17 @@ from pyquil.gates import X, Y, Z, H, CZ, CNOT, SWAP
 import numpy as np
 
 # Test programs with control flow but no loops
-class TestLinear(TestCase):
-    @pytest.mark.skip("Not implemented")
-    def test_simple(self):
-        qc = get_qc("3q-qvm")
+@pytest.mark.skip("Not implemented")
+def test_simple():
+    qc = get_qc("3q-qvm")
 
-        # TODO: How should we handle this case?
-        # |0, 0, 0>  -->  |?, ?, ?>
-        pq = Program(H(0))
-        ro = pq.declare("ro", "BIT", 1)
-        pq.measure(0, ro)
-        pq.if_then(ro, X(1), X(2))
+    # TODO: How should we handle this case?
+    # |0, 0, 0>  -->  |?, ?, ?>
+    pq = Program(H(0))
+    ro = pq.declare("ro", "BIT", 1)
+    pq.measure(0, ro)
+    pq.if_then(ro, X(1), X(2))
 
-        wf = qdb.Qdb(qc, pq).do_tomography()
-        # TODO: What should the wavefunction look like?
-        assert False
+    wf = qdb.Qdb(qc, pq).do_tomography()
+    # TODO: What should the wavefunction look like?
+    assert False
--- qdb/tests/test_trim.py
+++ qdb/tests/test_trim.py
@@ -1,4 +1,3 @@
-from unittest import TestCase
 import pytest
 
 from pyquil import Program, get_qc
@@ -6,80 +5,85 @@ from pyquil.gates import X, Y, Z, H, CZ, CNOT, SWAP
 import qdb
 
 
-class TestTrim(TestCase):
-    def test_basic(self):
-        qc = get_qc("3q-qvm")
+@pytest.mark.parametrize("qubits", [[0], [1], [2], [0, 1], [0, 2], [1, 2]])
+def test_basic(qubits):
+    qc = get_qc("3q-qvm")
+    pq = Program([H(0), CNOT(0, 1), CNOT(1, 2)])
+    trimmed = qdb.Qdb(qc, pq).trim_program(qubits)
+    assert trimmed == pq
+
+
+def test_entangled():
+    def construct_program(trimmed):
         pq = Program([H(0), CNOT(0, 1), CNOT(1, 2)])
-        trimmed = qdb.Qdb(qc, pq).trim_program([0, 1, 2])
-        self.assertEqual(trimmed, pq)
+        pq.declare("ro", "BIT", 1)
+        pq.measure(0, "ro")
+        if trimmed:
+            pq.if_then("ro", X(0), X(1))
+        else:
+            pq.if_then("ro", Program([X(0), H(3)]), Program([X(1), CZ(3, 4)]))
+        return pq
 
-    def test_entangled(self):
-        def construct_program(trimmed):
-            pq = Program([H(0), CNOT(0, 1), CNOT(1, 2)])
-            pq.declare("ro", "BIT", 1)
-            pq.measure(0, "ro")
-            if trimmed:
-                pq.if_then("ro", X(0), X(1))
-            else:
-                pq.if_then("ro", Program([X(0), H(3)]), Program([X(1), CZ(3, 4)]))
-            return pq
+    qc = get_qc("3q-qvm")
+    trimmed = qdb.Qdb(qc, construct_program(False)).trim_program([0, 1, 2])
+    assert trimmed == construct_program(True)
 
-        qc = get_qc("3q-qvm")
-        trimmed = qdb.Qdb(qc, construct_program(False)).trim_program([0, 1, 2])
-        self.assertEqual(trimmed, construct_program(True))
 
-    @pytest.mark.skip("Not implemented")
-    def test_if_then(self):
-        def construct_program(trimmed):
-            pq = Program(H(0))
-            pq.declare("ro", "BIT", 1)
-            pq.measure(0, "ro")
-            if not trimmed:
-                pq.if_then("ro", X(1), Y(1))
-            return pq
-
-        qc = get_qc("3q-qvm")
-        trimmed = qdb.Qdb(qc, construct_program(False)).trim_program([0])
-        self.assertEqual(trimmed, construct_program(True))
-
-    @pytest.mark.skip("Not implemented")
-    def test_simple_control_flow_dependency(self):
+@pytest.mark.skip("Not implemented")
+def test_if_then():
+    def construct_program(trimmed):
         pq = Program(H(0))
+        pq.declare("ro", "BIT", 1)
+        pq.measure(0, "ro")
+        if not trimmed:
+            pq.if_then("ro", X(1), Y(1))
+        return pq
+
+    qc = get_qc("3q-qvm")
+    trimmed = qdb.Qdb(qc, construct_program(False)).trim_program([0])
+    assert trimmed == construct_program(True)
+
+
+@pytest.mark.skip("Not implemented")
+def test_simple_control_flow_dependency():
+    pq = Program(H(0))
+    ro = pq.declare("ro")
+    pq.measure(0, ro)
+    pq.if_then(ro, X(1))
+
+    qc = get_qc("2q-qvm")
+    trimmed = qdb.Qdb(qc, pq).trim_program([1])
+    assert trimmed == pq
+
+
+@pytest.mark.skip("Not implemented")
+def test_control_flow_dependency():
+    def construct_program(trimmed):
+        pq = Program(H(0), H(2), CNOT(0, 1), CNOT(2, 3), X(0), X(2))
         ro = pq.declare("ro")
-        pq.measure(0, ro)
-        pq.if_then(ro, X(1))
+        pq.measure(3, ro)
+        if trimmed:
+            pq.if_then(ro, X(1))
+        else:
+            pq.if_then(ro, Program(X(2), X(1)))
+        return pq
 
-        qc = get_qc("2q-qvm")
-        trimmed = qdb.Qdb(qc, pq).trim_program([1])
-        self.assertEqual(trimmed, pq)
+    qc = get_qc("4q-qvm")
+    trimmed = qdb.Qdb(qc, construct_program(False)).trim_program([1])
+    assert trimmed == construct_program(True)
 
-    @pytest.mark.skip("Not implemented")
-    def test_control_flow_dependency(self):
-        def construct_program(trimmed):
-            pq = Program(H(0), H(2), CNOT(0, 1), CNOT(2, 3), X(0), X(2))
+
+@pytest.mark.skip("Not implemented")
+def test_trim_while_loop():
+    def construct_program(trimmed):
+        pq = Program(H(0))
+        if not trimmed:
+            pq += Program(H(1))
             ro = pq.declare("ro")
-            pq.measure(3, ro)
-            if trimmed:
-                pq.if_then(ro, X(1))
-            else:
-                pq.if_then(ro, Program(X(2), X(1)))
-            return pq
+            pq.measure(1, ro)
+            pq.while_do(ro, Program(X(1)).measure(1, ro))
+        pq += Program(X(0))
 
-        qc = get_qc("4q-qvm")
-        trimmed = qdb.Qdb(qc, construct_program(False)).trim_program([1])
-        self.assertEqual(trimmed, construct_program(True))
-
-    @pytest.mark.skip("Not implemented")
-    def test_trim_while_loop(self):
-        def construct_program(trimmed):
-            pq = Program(H(0))
-            if not trimmed:
-                pq += Program(H(1))
-                ro = pq.declare("ro")
-                pq.measure(1, ro)
-                pq.while_do(ro, Program(X(1)).measure(1, ro))
-            pq += Program(X(0))
-
-        qc = get_qc("2q-qvm")
-        trimmed = qdb.Qdb(qc, construct_program(False)).trim_program([0])
-        self.assertEqual(trimmed, construct_program(True))
+    qc = get_qc("2q-qvm")
+    trimmed = qdb.Qdb(qc, construct_program(False)).trim_program([0])
+    assert trimmed == construct_program(True)
